2aec85ccb5b24834b7c9d5db95df5cde19b081f2
carniatto <mateus.carniato@realimpactanalytics.com>
2020-08-07 11:20:13 +0300

    feat(userSeeker): added header and footer

--- apps/user-seeker/src/app/app.module.ts
+++ apps/user-seeker/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 import { RoutingModule } from './routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, RoutingModule],
   providers: [],
   bootstrap: [AppComponent],
